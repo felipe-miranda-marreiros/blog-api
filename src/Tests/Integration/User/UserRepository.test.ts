9ccba98308d3f6e286b81b6d63d00940c3fa67ff
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-08 13:02:21 -0300

    test: ensure UserSQLRepository.isEmailInUse() returns correct values

--- src/Tests/Integration/User/UserRepository.test.ts
+++ src/Tests/Integration/User/UserRepository.test.ts
@@ -1,6 +1,14 @@
+import { db } from '@/Infrastructure/Database/Drizzle/DrizzleClient'
 import { UserSQLRepository } from '@/Infrastructure/Database/Repositories/UserSQLRepository'
+import { emails } from '@/Infrastructure/Database/Schemas/Schemas'
 
 describe('User Repository', () => {
+  let userSQLRepository = null as unknown as UserSQLRepository
+
+  beforeAll(() => {
+    userSQLRepository = new UserSQLRepository()
+  })
+
   it('Should insert an User into database and return its model', async () => {
     const createUserMock = {
       email: 'any_email@gmail.com',
@@ -9,10 +17,25 @@ describe('User Repository', () => {
       password: 'any_password',
       username: 'any_username'
     }
-    const userSQLRepository = new UserSQLRepository()
     const user = await userSQLRepository.createUser(createUserMock)
     expect(user.first_name).toEqual(createUserMock.first_name)
     expect(user.last_name).toEqual(createUserMock.last_name)
     expect(user).not.toHaveProperty('password')
+  })
+  it('Should return false if isEmailInUse does not find an email equals to params', async () => {
+    const isEmailInUse = await userSQLRepository.isEmailInUse(
+      'avaiable_email@gmail.com'
+    )
+    expect(isEmailInUse).toEqual(false)
+  })
+  it('Should return true if isEmailInUse does find an email equals to params', async () => {
+    const usedEmail = await db
+      .insert(emails)
+      .values({ email: 'used_email@gmail.com' })
+      .returning({ usedEmail: emails.email })
+    const isEmailInUse = await userSQLRepository.isEmailInUse(
+      usedEmail[0].usedEmail
+    )
+    expect(isEmailInUse).toEqual(true)
   })
 })
