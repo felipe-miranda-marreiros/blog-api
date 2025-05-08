924e7867fdef08caffbe6f5617cde2623ac13527
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-08 11:35:48 -0300

    test: ensure SignUpUseCase return a jwt token

--- src/Tests/Unit/User/SignUpUseCase.spec.ts
+++ src/Tests/Unit/User/SignUpUseCase.spec.ts
@@ -1,10 +1,13 @@
+import { Encrypter } from '@/Application/Contracts/Criptography/Encrypter'
 import { HashComparer } from '@/Application/Contracts/Criptography/HashComparer'
 import { Hasher } from '@/Application/Contracts/Criptography/Hasher'
 import { ConflictError } from '@/Application/Contracts/Errors/ConflictError'
 import { UserRepository } from '@/Application/Contracts/Repositories/UserRepository'
 import { SignUpUseCase } from '@/Application/Modules/Users/UseCases/SignUpUseCase/SignUpUseCase'
+import { createEncrypterStub } from '@/Tests/Mocks/Criptography/EncrypterMock'
 import { createHasherStub } from '@/Tests/Mocks/Criptography/HasherMock'
 import {
+  createUserRepositoryMock,
   signUpParamsMock,
   signUpResponseMock
 } from '@/Tests/Mocks/Domain/User/UseCases'
@@ -13,17 +16,20 @@ import { createUserRepositoryStub } from '@/Tests/Mocks/Repositories/UserReposit
 interface Sut {
   sut: SignUpUseCase
   userRepositoryStub: UserRepository
-  encrypterStub: Hasher & HashComparer
+  hasherStub: Hasher & HashComparer
+  encrypterStub: Encrypter
 }
 
 function createSut(): Sut {
-  const userRepositoryStub = createUserRepositoryStub(signUpResponseMock)
-  const encrypterStub = createHasherStub()
-  const sut = new SignUpUseCase(userRepositoryStub, encrypterStub)
+  const userRepositoryStub = createUserRepositoryStub(createUserRepositoryMock)
+  const hasherStub = createHasherStub()
+  const encrypterStub = createEncrypterStub()
+  const sut = new SignUpUseCase(userRepositoryStub, hasherStub, encrypterStub)
   return {
     sut,
     userRepositoryStub,
-    encrypterStub
+    encrypterStub,
+    hasherStub
   }
 }
 
@@ -39,18 +45,30 @@ describe('SignUp UseCase', () => {
   it('Should create an User if params are valid', async () => {
     const { sut } = createSut()
     const response = await sut.signUp(signUpParamsMock)
-    expect(response).toEqual(response)
+    expect(response).toEqual(signUpResponseMock)
   })
   it('Should throw if Hasher throws', async () => {
-    const { sut, encrypterStub } = createSut()
-    jest.spyOn(encrypterStub, 'hash').mockRejectedValueOnce(new Error())
+    const { sut, hasherStub } = createSut()
+    jest.spyOn(hasherStub, 'hash').mockRejectedValueOnce(new Error())
     const promise = sut.signUp(signUpParamsMock)
     await expect(promise).rejects.toThrow()
   })
   it('Should call Hasher witch correct values', async () => {
-    const { sut, encrypterStub } = createSut()
-    const hashedPassword = jest.spyOn(encrypterStub, 'hash')
+    const { sut, hasherStub } = createSut()
+    const hashedPassword = jest.spyOn(hasherStub, 'hash')
     await sut.signUp(signUpParamsMock)
     expect(hashedPassword).toHaveBeenCalledWith(signUpParamsMock.password)
+  })
+  it('Should call Encryper witch correct values', async () => {
+    const { sut, encrypterStub } = createSut()
+    const encrypter = jest.spyOn(encrypterStub, 'encrypt')
+    await sut.signUp(signUpParamsMock)
+    expect(encrypter).toHaveBeenCalledWith(createUserRepositoryMock)
+  })
+  it('Should throw if Encryper throws', async () => {
+    const { sut, encrypterStub } = createSut()
+    jest.spyOn(encrypterStub, 'encrypt').mockRejectedValueOnce(new Error())
+    const promise = sut.signUp(signUpParamsMock)
+    await expect(promise).rejects.toThrow()
   })
 })
