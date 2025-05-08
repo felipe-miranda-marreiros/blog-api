72aab6d9c830ecd84974fc1c0d0ce6cec3247ed6
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-08 00:25:04 -0300

    refactor: update CreateUser Controller test

--- src/Presentation/Controllers/CreateUserController/CreateUserController.ts
+++ src/Presentation/Controllers/CreateUserController/CreateUserController.ts
@@ -8,14 +8,19 @@ import {
   HttpRequest,
   HttpResponse
 } from '@/Presentation/Contracts/Controller'
+import { Validation } from '@/Presentation/Contracts/Validation'
 
 export class CreateUserController implements Controller {
-  constructor(private readonly createUserUseCase: CreateUser) {}
+  constructor(
+    private readonly createUserUseCase: CreateUser,
+    private readonly validation: Validation
+  ) {}
 
   async handle(
     request: HttpRequest<CreateUserParams>
   ): Promise<HttpResponse<CreateUserResponse>> {
-    const user = await this.createUserUseCase.createUser(request.body)
+    const result = this.validation.validate(request.body)
+    const user = await this.createUserUseCase.createUser(result)
     return {
       status_code: 200,
       body: user
--- src/Tests/Unit/User/CreateUserController.spec.ts
+++ src/Tests/Unit/User/CreateUserController.spec.ts
@@ -3,21 +3,51 @@ import {
   CreateUserResponse
 } from '@/Domain/Users/Models/User'
 import { CreateUser } from '@/Domain/Users/UseCases/CreateUser'
+import { Validation } from '@/Presentation/Contracts/Validation'
 import { CreateUserController } from '@/Presentation/Controllers/CreateUserController/CreateUserController'
 import {
   createUserParamsMock,
   createUserResponseMock
 } from '@/Tests/Mocks/Domain/User/UseCases'
 
+interface Sut {
+  validationStub: Validation
+  createUserUseCaseStub: CreateUser
+  sut: CreateUserController
+}
+
+function makeCreateUserUseCase(): CreateUser {
+  class CreateUserUseCaseStub implements CreateUser {
+    createUser(params: CreateUserParams): Promise<CreateUserResponse> {
+      return Promise.resolve(createUserResponseMock)
+    }
+  }
+  return new CreateUserUseCaseStub()
+}
+
+function makeValidation(): Validation {
+  class ValidationStub implements Validation {
+    validate<TData>(data: TData): TData {
+      return data
+    }
+  }
+  return new ValidationStub()
+}
+
+function createSut(): Sut {
+  const validationStub = makeValidation()
+  const createUserUseCaseStub = makeCreateUserUseCase()
+  const sut = new CreateUserController(createUserUseCaseStub, validationStub)
+  return {
+    createUserUseCaseStub,
+    validationStub,
+    sut
+  }
+}
+
 describe('CreateUser Controller', () => {
   it('Should return 200 if User is created successfully', async () => {
-    class CreateUserUseCaseStub implements CreateUser {
-      createUser(params: CreateUserParams): Promise<CreateUserResponse> {
-        return Promise.resolve(createUserResponseMock)
-      }
-    }
-    const createUserUseCaseStub = new CreateUserUseCaseStub()
-    const sut = new CreateUserController(createUserUseCaseStub)
+    const { sut } = createSut()
     const response = await sut.handle({ body: createUserParamsMock })
     expect(response.body?.first_name).toEqual(createUserResponseMock.first_name)
     expect(response.body?.last_name).toEqual(createUserResponseMock.last_name)
