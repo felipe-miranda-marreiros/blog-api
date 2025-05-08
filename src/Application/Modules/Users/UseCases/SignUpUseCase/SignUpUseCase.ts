3bda9f3768e21c2fe3891f85f0b41203ddaaa4e7
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-08 09:54:47 -0300

    feat: add Hasher to SignUpUseCase

--- src/Application/Modules/Users/UseCases/SignUpUseCase/SignUpUseCase.ts
+++ src/Application/Modules/Users/UseCases/SignUpUseCase/SignUpUseCase.ts
@@ -1,10 +1,14 @@
+import { Hasher } from '@/Application/Contracts/Criptography/Hasher'
 import { ConflictError } from '@/Application/Contracts/Errors/ConflictError'
 import { UserRepository } from '@/Application/Contracts/Repositories/UserRepository'
 import { SignUpParams, SignUpResponse } from '@/Domain/Users/Models/User'
 import { SignUp } from '@/Domain/Users/UseCases/SignUp'
 
 export class SignUpUseCase implements SignUp {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly encrypter: Hasher
+  ) {}
 
   async signUp(params: SignUpParams): Promise<SignUpResponse> {
     const isEmailOrUsernameInUse =
@@ -13,11 +17,19 @@ export class SignUpUseCase implements SignUp {
         username: params.username
       })
 
+    const hashedPassword = await this.encrypter.hash(params.password)
+
     if (isEmailOrUsernameInUse) {
       throw new ConflictError('User already exits with email or username')
     }
 
-    const user = await this.userRepository.createUserRespository(params)
+    const user = await this.userRepository.createUserRespository({
+      email: params.email,
+      first_name: params.first_name,
+      last_name: params.last_name,
+      password: hashedPassword,
+      username: params.username
+    })
 
     return user
   }
