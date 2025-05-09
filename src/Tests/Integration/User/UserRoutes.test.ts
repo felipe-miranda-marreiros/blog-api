ba2f32ac5fd5204f181e3caff093bb9da30b2cfc
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-08 21:52:40 -0300

    test: should return 200 and a cookie on sign up

--- src/Tests/Integration/User/UserRoutes.test.ts
+++ src/Tests/Integration/User/UserRoutes.test.ts
@@ -16,5 +16,23 @@ describe('User Routes', () => {
         .set('Accept', 'application/json')
         .expect(200)
     })
+
+    it('Should return 200 and a cookie on sign up', async () => {
+      const response = await request(app)
+        .post('/api/sign-up')
+        .send({
+          email: 'cookie_email@gmail.com',
+          password: 'cookie_password',
+          username: 'any_username4847778',
+          last_name: 'any_last_name',
+          first_name: 'any_first_name'
+        })
+        .expect(200)
+
+      const cookies = response.headers['set-cookie'] as unknown as string[]
+      const cookie = cookies.find((cookie) => cookie.startsWith('jwt'))
+      const accessToken = cookie?.split(';')[0].split('=')[1]
+      expect(accessToken).toBeDefined()
+    })
   })
 })
