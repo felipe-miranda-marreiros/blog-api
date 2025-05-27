4bb112f733ee9c3b9594dfe2400c758b6c35542a
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2025-05-27 14:16:51 -0300

    refactor: update env examples

--- src/Main/Dependencies/Infrastructure.ts
+++ src/Main/Dependencies/Infrastructure.ts
@@ -1,5 +1,6 @@
 import { BcryptAdapter } from '@/Infrastructure/Cryptography/BcryptAdapter'
 import { JwtAdapter } from '@/Infrastructure/Cryptography/JwtAdapter'
+import { ENV } from '@/Shared/Env/Env'
 
 export const bcryptAdapter = new BcryptAdapter(12)
-export const jwtAdapter = new JwtAdapter('jwt_secret')
+export const jwtAdapter = new JwtAdapter(ENV.JWT_SECRET)
--- src/Main/index.ts
+++ src/Main/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import { app } from './Libs/Express/Express'
+import { ENV } from '@/Shared/Env/Env'
 
-app.listen(3000, () => {
+app.listen(ENV.APP_PORT, () => {
   console.log(`Server running on port 3000`)
 })
